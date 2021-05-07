78f05ce8e6261f07f03b6fdd01fa96dad0f150a9
Marina Russ <marina@cameystudios.com>
2021-05-07 16:47:08 -0400

    adds connection.js file

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+const mysql = require('mysql');
+const inquirer = require('requirer');
+
+require('dotenv').config();
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: process.env.MYSQL_PASSWORD,
+  database: 'employees'
+});
+
+module.exports = connection;
